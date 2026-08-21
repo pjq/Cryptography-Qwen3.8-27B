5342926bcbe5c6a0d120c99ddeb5a872d7677217
Peng Jianqing <pengjianqing@gmail.com>
2026-08-21 17:34:45 +0800

    Compress repeated AES rounds with round selector

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cryptoworld-v25-vertical-aes-flow';
+const CACHE = 'cryptoworld-v26-compressed-aes-rounds';
 const ASSETS = ['.', 'index.html', 'manifest.json', 'css/course.css', 'css/google-ui.css',
   'js/curriculum.js', 'js/course-content.js', 'js/ciphers.js', 'js/course-app.js',
   'icons/icon-192.png', 'icons/icon-512.png'];
