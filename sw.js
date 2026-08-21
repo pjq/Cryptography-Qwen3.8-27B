3e462cfd09be0e20674e0d7a8119d11bb4520824
Peng Jianqing <pengjianqing@gmail.com>
2026-08-21 16:56:09 +0800

    Restore AES state matrix renderer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cryptoworld-v23-aes-full-round-trace';
+const CACHE = 'cryptoworld-v24-aes-matrix-fix';
 const ASSETS = ['.', 'index.html', 'manifest.json', 'css/course.css', 'css/google-ui.css',
   'js/curriculum.js', 'js/course-content.js', 'js/ciphers.js', 'js/course-app.js',
   'icons/icon-192.png', 'icons/icon-512.png'];
