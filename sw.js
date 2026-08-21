6e3a92c17db3a7e2e8c777cc63fb62639f84ee80
Peng Jianqing <pengjianqing@gmail.com>
2026-08-21 12:25:05 +0800

    Add topic calculations to practical labs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cryptoworld-v10-input-output-labs';
+const CACHE = 'cryptoworld-v11-topic-lab-calculations';
 const ASSETS = ['.', 'index.html', 'manifest.json', 'css/course.css', 'css/google-ui.css',
   'js/curriculum.js', 'js/course-content.js', 'js/ciphers.js', 'js/course-app.js',
   'icons/icon-192.png', 'icons/icon-512.png'];
