90cc0807998c7740bc474c305c604594fbaa211a
Peng Jianqing <pengjianqing@gmail.com>
2026-08-21 11:31:13 +0800

    Author detailed content and labs for all 35 lessons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cryptoworld-v7-detailed-content';
+const CACHE = 'cryptoworld-v8-full-course-content';
 const ASSETS = ['.', 'index.html', 'manifest.json', 'css/course.css', 'css/google-ui.css',
   'js/curriculum.js', 'js/course-content.js', 'js/ciphers.js', 'js/course-app.js',
   'icons/icon-192.png', 'icons/icon-512.png'];
