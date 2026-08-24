3048720407c93f35335cbe6bcc867d181db2b7d8
Peng Jianqing <pengjianqing@gmail.com>
2026-08-24 14:04:11 +0800

    Merge AES round chips into strip

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cryptoworld-v31-aes-avalanche';
+const CACHE = 'cryptoworld-v32-single-round-selector';
 const ASSETS = ['.', 'index.html', 'manifest.json', 'css/course.css', 'css/google-ui.css',
   'js/curriculum.js', 'js/course-content.js', 'js/ciphers.js', 'js/course-app.js',
   'icons/icon-192.png', 'icons/icon-512.png'];
