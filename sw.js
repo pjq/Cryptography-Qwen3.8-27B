b77be57ecb66b62d151fe8eb47fbaacd6e8b41fc
Peng Jianqing <pengjianqing@gmail.com>
2026-08-25 12:25:57 +0800

    Add animated DH man-in-the-middle lab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cryptoworld-v34-gcm-auth-lab';
+const CACHE = 'cryptoworld-v35-mitm-lab';
 const ASSETS = ['.', 'index.html', 'manifest.json', 'css/course.css', 'css/google-ui.css',
   'js/curriculum.js', 'js/course-content.js', 'js/ciphers.js', 'js/course-app.js',
   'icons/icon-192.png', 'icons/icon-512.png'];
